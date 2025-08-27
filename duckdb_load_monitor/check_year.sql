1595f788c6a6a16ddccd1fda9cc6085e7a41d1cf
ym <stirlitz@yms-MacBook-Pro.local>
2025-08-27 12:11:43 -0400

    added data load monitoring querries folder

---
+++ duckdb_load_monitor/check_year.sql
@@ -0,0 +1,10 @@
+duckdb -c "
+ SELECT   stocks_id,
+ MIN(c_date) AS min_d,
+     MAX(c_date) AS max_d,
+     COUNT(*)    AS n
+  FROM read_parquet('$IVOL_DATA_DIR/raw/*.parquet')
+  WHERE c_date >= '2006-01-01'
+    GROUP BY stocks_id
+    ORDER BY n DESC;
+    "
