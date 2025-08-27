ff358797c0a3cac35c656bb27e5cefd638f5e7b2
ym <stirlitz@yms-MacBook-Pro.local>
2025-08-27 13:59:00 -0400

    fixed an error in sql/04_curve_header.sql'

--- sql/04_curve_header.sql
+++ sql/04_curve_header.sql
@@ -2,7 +2,7 @@ COPY (
 SELECT
   A.stocks_id, A.c_date, A.expiration_date, A.S, A.tau, A.iv_atm,
   S.slope
-FROM read_parquet('$IVOL_DATA_DIR/curated/atms${YEAR_SUFFIX}.parquet') A
+FROM read_parquet('$IVOL_DATA_DIR/curated/atm${YEAR_SUFFIX}.parquet') A
 LEFT JOIN read_parquet('$IVOL_DATA_DIR/curated/smile_slopes${YEAR_SUFFIX}.parquet') S
 USING (stocks_id, c_date, expiration_date)
 ) TO '$IVOL_DATA_DIR/curated/curve_headers${YEAR_SUFFIX}.parquet'
